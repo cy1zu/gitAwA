943e9fef71927a381e10a8c3415aaabc54c6c637
chIyo1zu <cy1zuofficial@gamil.com>
2024-10-25 16:12:19 +0800

    v0.0.1 项目脚手架搭建

---
+++ backend/config/config.go
@@ -0,0 +1,62 @@
+package config
+
+import (
+	"fmt"
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+)
+
+var Conf = new(AppConfig)
+
+type AppConfig struct {
+	Name            string `mapstructure:"name"`
+	Mode            string `mapstructure:"mode"`
+	Version         string `mapstructure:"version"`
+	Port            string `mapstructure:"port"`
+	StartTime       string `mapstructure:"start_time"`
+	MachineID       int64  `mapstructure:"machine_id"`
+	*LogConfig      `mapstructure:"log"`
+	*PostgresConfig `mapstructure:"Postgres"`
+}
+
+type LogConfig struct {
+	Mode       string `mapstructure:"mode"`
+	Level      string `mapstructure:"level"`
+	Filename   string `mapstructure:"filename"`
+	MaxSize    int    `mapstructure:"max_size"`
+	MaxBackups int    `mapstructure:"max_backups"`
+	MaxAge     int    `mapstructure:"max_age"`
+}
+
+type PostgresConfig struct {
+	Host     string `mapstructure:"host"`
+	Port     string `mapstructure:"port"`
+	User     string `mapstructure:"user"`
+	Password string `mapstructure:"password"`
+	Database string `mapstructure:"database"`
+	MaxConns int    `mapstructure:"max_conns"`
+	MaxIdle  int    `mapstructure:"max_idle"`
+}
+
+func Init() (err error) {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	err = viper.ReadInConfig()
+	if err != nil {
+		fmt.Printf("viper.ReadInConfig() failed, err:%v\n", err)
+		return
+	}
+	if err := viper.Unmarshal(Conf); err != nil {
+		fmt.Printf("viper.Unmarshal() failed, err:%v\n", err)
+		return err
+	}
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Printf("Config file changed: %s\n", e.Name)
+		if err := viper.Unmarshal(Conf); err != nil {
+			fmt.Printf("viper.Unmarshal() failed, err:%v\n", err)
+		}
+	})
+	return err
+}
